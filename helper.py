8e4c86036b68a5f235087f8b435ed6cbfa93e647
srishti0017 <115053826+srishti0017@users.noreply.github.com>
2022-11-07 13:36:09 +0530

    Create helper.py

---
+++ helper.py
@@ -0,0 +1,85 @@
+from urlextract import URLExtract
+
+from wordcloud import WordCloud
+
+extract = URLExtract()
+
+def fetch_stats(selected_user, df):
+
+    if selected_user != 'Overall':
+
+        df = df[df['user'] == selected_user]
+
+    num_messages = df.shape[0]
+
+    words = []
+
+    for message in df['message']:
+
+         words.extend(message.split())
+
+    # if selected_user == 'Overall':
+
+    #     #1st fetch no. of messages
+
+    #     num_messages = df.shape[0]
+
+    #     #2nd no.of words
+
+    #     words = []
+
+    #     for message in df['message']:
+
+    #         words.extend(message.split())
+
+    #     return num_messages,len(words)
+
+    # else:
+
+    #     new_df = df[df['user'] == selected_user]
+
+    #     num_messages = new_df.shape[0]
+
+    #     words = []
+
+    #     for message in new_df['message']:
+
+    #         words.extend(message.split())
+
+    #     return num_messages,len(words)
+
+    # fetch no. of media messages
+
+    num_media_messages = df[df['message'] == '<Media omitted>\n'].shape[0]
+
+    # fetch no. of links
+
+    links = []
+
+    for message in df['message']:
+
+        links.extend(extract.find_urls(message))
+
+    return num_messages, len(words),num_media_messages,len(links)
+
+def most_busy_user(df):
+
+    x = df['user'].value_counts().head()
+
+    df = round((df['user'].value_counts() / df.shape[0]) * 100, 2).reset_index().rename(
+
+        columns={'index': 'name', 'user': 'percent'})
+
+    return x,df
+
+def create_wordcloud(selected_user, df):
+
+    if selected_user != 'Overall':
+
+        df = df[df['user'] == selected_user]
+
+    wc = WordCloud(width=500, height=500, min_font_size=10, background_color='white')
+
+    df_wc = wc.generate(df['message'].str.cat(sep=" "))
+
+    return df_wc
